42532d88727e3e4ab242d9d03f1cbc2631aef9d1
Kevin Grellus <k_grellus@taures.de>
2021-12-02 14:31:32 +0100

    add day 1 and day 2

---
+++ 01/main.py
@@ -0,0 +1,27 @@
+from shared import load_inputs
+
+
+def task1(task_input):
+    depths = [int(line) for line in task_input]
+    increases = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i - 1]:
+            increases += 1
+    return increases
+
+
+def task2(task_input):
+    depths = [int(line) for line in task_input]
+    increases = 0
+    for i in range(0, len(depths) - 2):
+        window = sum(depths[i:i + 3])
+        next_window = sum(depths[i + 1:i + 4])
+        if next_window > window:
+            increases += 1
+    return increases
+
+
+if __name__ == '__main__':
+    example_input, puzzle_input = load_inputs(__file__)
+    print(f'task 1 (number of increases): {task1(puzzle_input)}')
+    print(f'task 2 (number of increases of windows): {task2(puzzle_input)}')
---
+++ 02/main.py
@@ -0,0 +1,48 @@
+import re
+from shared import load_inputs
+
+
+def task1(instructions):
+    horizontal, depth = 0, 0
+    for instruction in instructions:
+        match = re.match('forward (\d+)', instruction)
+        if match is not None:
+            horizontal += int(match.group(1))
+            continue
+        match = re.match('down (\d+)', instruction)
+        if match is not None:
+            depth += int(match.group(1))
+            continue
+        match = re.match('up (\d+)', instruction)
+        if match is not None:
+            depth -= int(match.group(1))
+            continue
+    return horizontal, depth
+
+
+def task2(instructions):
+    horizontal, depth, aim = 0, 0, 0
+    for instruction in instructions:
+        match = re.match('forward (\d+)', instruction)
+        if match is not None:
+            x = int(match.group(1))
+            horizontal += x
+            depth += aim * x
+            continue
+        match = re.match('down (\d+)', instruction)
+        if match is not None:
+            aim += int(match.group(1))
+            continue
+        match = re.match('up (\d+)', instruction)
+        if match is not None:
+            aim -= int(match.group(1))
+            continue
+    return horizontal, depth
+
+
+if __name__ == '__main__':
+    example_input, puzzle_input = load_inputs(__file__)
+    task1_horizontal, task1_depth = task1(puzzle_input)
+    print(f'task 1: horizontal:{task1_horizontal}, depth:{task1_depth}, solution:{task1_horizontal * task1_depth}')
+    task2_horizontal, task2_depth = task2(puzzle_input)
+    print(f'task 2: horizontal:{task2_horizontal}, depth:{task2_depth}, solution:{task2_horizontal * task2_depth}')

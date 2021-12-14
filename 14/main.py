9716f4526f501fd1e13b1ff292697d4ad0712736
Kevin Grellus <k_grellus@taures.de>
2021-12-15 00:27:46 +0100

    implement fast 14.2

--- 14/main.py
+++ 14/main.py
@@ -1,6 +1,8 @@
 import re
 import sys
 import time
+from collections import defaultdict
+
 
 sys.path.append('../')
 from shared import load_inputs
@@ -83,13 +85,47 @@ def task1(task_input: [str]) -> int:
 
 
 def task2(task_input: [str]) -> int:
-    return 0
+    polymer_str = task_input[0]
+    changes_str = task_input[2:]
+    changes = []
+    for change in changes_str:
+        changes.append(re.match('(\w+) -> (\w)', change).groups())
+    adjacent_atoms = defaultdict(lambda: 0, {})
+    for i in range(len(polymer_str) - 1):
+        key = polymer_str[i] + polymer_str[i + 1]
+        adjacent_atoms[key] += 1
+    for _ in range(40):
+        new_adjacent_atoms = defaultdict(lambda: 0, {})
+        for sequence, atom in changes:
+            if sequence in adjacent_atoms:
+                a = sequence[0] + atom
+                b = atom + sequence[1]
+                new_adjacent_atoms[a] += adjacent_atoms[sequence]
+                new_adjacent_atoms[b] += adjacent_atoms[sequence]
+                adjacent_atoms.pop(sequence)
+        adjacent_atoms = new_adjacent_atoms
+    atom_counts = defaultdict(lambda: 0, {})
+    atom_counts[polymer_str[0]] = 1
+    atom_counts[polymer_str[-1]] = 1
+    for sequence in adjacent_atoms:
+        atom_counts[sequence[0]] += adjacent_atoms[sequence]
+        atom_counts[sequence[1]] += adjacent_atoms[sequence]
+    for key in atom_counts:
+        atom_counts[key] = int(atom_counts[key]/2)  # because every character is in 2 sequences (left and right)
+    min_atoms, max_atoms = min(atom_counts.values()), max(atom_counts.values())
+    return max_atoms - min_atoms
 
 
 def main():
     example_input, puzzle_input = load_inputs(__file__)
-    print(f'task 1: {task1(puzzle_input)}')
-    print(f'task 2: {task2(example_input)}')
+    tic = time.perf_counter()
+    task1_solution = task1(puzzle_input)
+    toc = time.perf_counter()
+    print(f'task 1: with slow calculation (after 10 steps) = {task1_solution} after {toc - tic:0.4f} seconds')
+    tic = time.perf_counter()
+    task2_solution = task2(puzzle_input)
+    toc = time.perf_counter()
+    print(f'task 2: with fast calculation (after 40 steps) = {task2_solution} after {toc - tic:0.4f} seconds')
 
 
 if __name__ == '__main__':
